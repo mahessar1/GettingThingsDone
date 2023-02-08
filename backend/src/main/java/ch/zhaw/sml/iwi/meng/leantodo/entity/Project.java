383b6d13078af11aa204ac481b06d41b4d8f6c42
Patrick Häusler <haeuspa1@students.zhaw.ch>
2023-02-08 11:23:18 +0100

    Test

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/entity/Project.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/entity/Project.java
@@ -1,31 +1,29 @@
 package ch.zhaw.sml.iwi.meng.leantodo.entity;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class Task {
+public class Project {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String description;
-    private LocalDateTime created;
-    private String status;
-    private String category;
-    private LocalDate dueDate;
-    @ManyToOne
-    private List list;
-    
-   
 
+    private String title;
+
+    private String owner;
+
+    @OneToMany
+    private List<ToDo> toDos = new ArrayList<>();
+    
 }
