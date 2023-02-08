fde4c1b41ca127dc306ff92119a62444f09abc34
Patrick Häusler <haeuspa1@students.zhaw.ch>
2023-02-08 10:55:09 +0100

    Task entity and repository created

---
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/entity/TaskRepository.java
@@ -0,0 +1,7 @@
+package ch.zhaw.sml.iwi.meng.leantodo.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+}
